a7a4d51a80eba33b9bd760a01af8daa0c6e06fc3
Aswanth Anoop <77261421+aswanthanoop@users.noreply.github.com>
2023-06-09 08:39:10 +0530

    LeetCode 217

---
+++ containsDuplicate.java
@@ -0,0 +1,15 @@
+public class containsDuplicate {
+    class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            HashSet<Integer> numbers =  new HashSet<>();
+    
+            for (int i=0; i<nums.length; i++) {
+                if(numbers.contains(nums[i])) return true;
+                numbers.add(nums[i]);
+    
+            }
+            
+            return false;
+        }
+    }
+}
