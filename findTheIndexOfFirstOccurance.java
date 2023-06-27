fdc82be8e7f86f559b15d98555753756f495579d
Aswanth Anoop <77261421+aswanthanoop@users.noreply.github.com>
2023-06-27 19:12:41 +0530

    LeetCode 28

---
+++ findTheIndexOfFirstOccurance.java
@@ -0,0 +1,11 @@
+public class findTheIndexOfFirstOccurance {
+    public int strStr(String haystack,String needle){
+        if(haystack.indexOf(needle) != 0){
+            return haystack.indexOf(needle);
+        }
+        return 0;
+        
+        
+    }
+    
+}
