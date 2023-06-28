f802d26e30ffdb986d9534af4de4843894536b33
Aswanth Anoop <77261421+aswanthanoop@users.noreply.github.com>
2023-06-28 16:45:18 +0530

    LeetCode 136

---
+++ singleNumber.java
@@ -0,0 +1,15 @@
+public class singleNumber {
+    class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        int result = 0;
+        for (int i=0;i<n;i++){
+            result^= nums[i];
+            
+        }
+        return result;
+        
+    }
+}
+    
+}
