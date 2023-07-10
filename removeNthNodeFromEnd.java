458b8154d4eede40d119b3d7dd9e47cce1bcaf95
aswanthanoop <aswanthanoop42@gmail.com>
2023-07-10 12:32:52 +0530

    LeetCode 19

---
+++ removeNthNodeFromEnd.java
@@ -0,0 +1,35 @@
+public class removeNthNodeFromEnd {
+   
+  
+public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+ 
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        for(int i = 1;i<=n+1;i++){
+            fast = fast.next;
+        }
+        while(fast != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummy.next;
+
+
+    }
+}
+    
+}
