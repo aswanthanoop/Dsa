53025095908f4fbbb34a15222a75f55184ccf19e
aswanthanoop <aswanthanoop42@gmail.com>
2023-07-11 20:59:35 +0530

    LeetCode 24

---
+++ swapNodesInPairs.java
@@ -0,0 +1,38 @@
+public class swapNodesInPairs {
+
+ 
+ public class ListNode {
+    int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+
+        while (current.next != null && current.next.next != null) {
+            ListNode firstNode = current.next;
+            ListNode secondNode = current.next.next;
+
+            
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+            current.next = secondNode;
+
+            current = current.next.next;
+        }
+
+        return dummy.next;
+    }
+}
+
+}
