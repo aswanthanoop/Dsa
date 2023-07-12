3636e726a6d0b9c1105903f2625a0a1a3dca91fd
aswanthanoop <aswanthanoop42@gmail.com>
2023-07-12 10:48:49 +0530

    LeetCode 20

--- ValidParenthisis.java
+++ ValidParenthisis.java
@@ -1,23 +1,25 @@
+import java.util.*;
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() % 2 != 0) return false;
-
-        Stack<Character> stack = new Stack();
+        Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-
-            if(c =='(' || c == '{' || c == '[') {
+            if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
-            } else if(c == ')' && !stack.isEmpty() && stack.peek() =='(' ) {
-                stack.pop();
-            } else if(c == '}' && !stack.isEmpty() && stack.peek() =='{' ) {
-                stack.pop();
-            } else if(c == ']' && !stack.isEmpty() && stack.peek() =='[' ) {
-                stack.pop();
-            } 
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
         }
-
-        return stack.isEmpty();
+        return stack.empty();
     }
-        
-    
 }
