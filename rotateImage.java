c14d5392122dcada875ec5bb8e71cf7b83d98214
aswanthanoop <aswanthanoop42@gmail.com>
2023-07-21 17:18:46 +0530

    LeetCode 48

---
+++ rotateImage.java
@@ -0,0 +1,32 @@
+public class rotateImage {
+    class Solution {
+        public void rotate(int[][] matrix) {
+            int n = matrix.length;
+            
+            //Column Reverse
+            for(int i = 0; i < n; i++){
+                int a = 0;
+                int b = n-1;
+                while(a <= b){
+                   int temp = matrix[a][i];
+                    matrix[a][i] = matrix[b][i];
+                     matrix[b][i] = temp;
+                     a++;
+                     b--;
+                }
+                
+            }
+               
+            //Transpose
+            for(int i = 0; i < n; i++){
+                for(int j =i+1; j< n; j++){
+                    int temp =  matrix[i][j];
+                     matrix[i][j] =  matrix[j][i];;
+                     matrix[j][i] = temp;
+                }
+            }
+            
+        }
+    }
+    
+}
