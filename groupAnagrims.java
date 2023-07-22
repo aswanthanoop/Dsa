30375409d9f8354ff3eb5d5d33fa4410122f38e5
aswanthanoop <aswanthanoop42@gmail.com>
2023-07-22 13:55:53 +0530

    LeetCode 49

---
+++ groupAnagrims.java
@@ -0,0 +1,47 @@
+public class groupAnagrims {
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            if(strs == null || strs.length == 0){
+                return new ArrayList<>();
+            }
+            Map<String,List<String>> frequencyStringMap = new HashMap<>();
+            for(String str : strs){
+                String frequencyString = getFrequencyString(str);
+    
+                if(frequencyStringMap.containsKey(frequencyString)){
+                    frequencyStringMap.get(frequencyString).add(str);
+                }
+                else{
+                    List<String> strList = new ArrayList<>();
+                    strList.add(str);
+                    frequencyStringMap.put(frequencyString,strList);
+                }
+            }
+    
+            return new ArrayList<>(frequencyStringMap.values());
+        }
+         private String getFrequencyString(String str) {
+    
+        
+        int[] freq = new int[26];
+    
+        
+        for (char c : str.toCharArray()) {
+          freq[c - 'a']++;
+        }
+    
+        
+        StringBuilder frequencyString = new StringBuilder("");
+        char c = 'a';
+        for (int i : freq) {
+          frequencyString.append(c);
+          frequencyString.append(i);
+          c++;
+        }
+    
+        return frequencyString.toString();
+      }
+        
+    }
+    
+}
