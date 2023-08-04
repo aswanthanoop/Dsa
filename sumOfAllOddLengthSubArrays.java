3a69d0e11ecb9ba9101de6fc36fec5208af9fb4c
aswanthanoop <aswanthanoop42@gmail.com>
2023-08-04 15:43:27 +0530

    LeetCode 1588

--- sumOfAllOddLengthSubArrays.java
+++ sumOfAllOddLengthSubArrays.java
@@ -1,19 +1,19 @@
 public class sumOfAllOddLengthSubArrays {
     class Solution {
-        public int sumOddLengthSubarrays(int[] arr) {
-            int n = arr.length,result = 0,start,end,total,odd;
-    
-            for(int i = 0; i < n; i++) {
-                end = i + 1;     
-                start = n - i; 
-                total = end * start;
-                odd = total / 2;    
-                if(total % 2 != 0)      
-                    odd++;                    
-                result += odd * arr[i]; 
-            }
-            return result;
+    public int sumOddLengthSubarrays(int[] arr) {
+        int n = arr.length,result = 0,start,end,total,odd;
+
+        for(int i = 0; i < n; i++) {
+            end = i + 1;     
+            start = n - i; 
+            total = end * start;
+            odd = total / 2;    
+            if(total % 2 != 0)      
+                odd++;                    
+            result += odd * arr[i]; 
         }
+        return result;
     }
+}
     
 }
