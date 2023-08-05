a4f08ed93cb41b960f64a4cf309415940c64542b
aswanthanoop <aswanthanoop42@gmail.com>
2023-08-05 15:11:29 +0530

    LeetCode 2798

---
+++ numberOfEmployeesWhoMetTheTarget.java
@@ -0,0 +1,16 @@
+public class numberOfEmployeesWhoMetTheTarget {
+    class Solution {
+        public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+            int count=0;
+            for(int i:hours)
+            {
+                if(i>=target)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+    
+}
