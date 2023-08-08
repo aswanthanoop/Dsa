9d068320ca76006b7e638d60952841af7a9ac805
aswanthanoop <aswanthanoop42@gmail.com>
2023-08-08 11:44:46 +0530

    LeetCode 134

--- gasStation.java
+++ gasStation.java
@@ -1,22 +1,22 @@
 public class gasStation {
     class Solution {
-        public int canCompleteCircuit(int[] gas, int[] cost) {
-            int total = 0;
-            int avail = 0;
-            int index = 0;
-            for(int i = 0;i<gas.length;i++){
-                total+= gas[i] - cost[i];
-                avail+= gas[i] - cost[i];
-                if(avail<0){
-                    avail = 0;
-                    index = i+1;
-                }
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0;
+        int avail = 0;
+        int index = 0;
+        for(int i = 0;i<gas.length;i++){
+            total+= gas[i] - cost[i];
+            avail+= gas[i] - cost[i];
+            if(avail<0){
+                avail = 0;
+                index = i+1;
             }
-            if(total<0){
-                return -1;
-            }
-            return index;
         }
+        if(total<0){
+            return -1;
+        }
+        return index;
     }
+}
     
 }
