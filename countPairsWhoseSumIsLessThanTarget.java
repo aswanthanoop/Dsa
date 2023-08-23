07da6698c272c841827ceae07f6ec6417e76c15b
Aswanth Anoop <aswanthanoop42@gmail.com>
2023-08-23 20:12:51 +0530

    LeetCode 2652

---
+++ countPairsWhoseSumIsLessThanTarget.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class countPairsWhoseSumIsLessThanTarget{
+    class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        int cnt = 0;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums.get(i) + nums.get(j) < target)  cnt++;
+            }
+        }
+        return cnt;
+    }
+}
+}
