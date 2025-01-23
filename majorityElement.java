640512bed7c9b46c2ce92b52de585812e1fd2d54
Aswanth Anoop <aswanthanoop42@gmail.com>
2025-01-23 07:50:31 +0530

    LeetCode 169

--- majorityElement.java
+++ majorityElement.java
@@ -1,16 +1,16 @@
 public class majorityElement {
     class Solution {
         public int majorityElement(int[] nums) {
-            int candidate = nums[0];
+            int majority = nums[0];
             int count = 0;
     
             for(int num : nums){
                 if (count ==0){
-                    candidate = num;
+                    majority = num;
                 }
-                count += (num == candidate) ? 1 :-1;
+                count += (num == majority) ? 1 :-1;
             }
-            return candidate;
+            return majority;
         }
     }
 }
